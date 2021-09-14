87f28997aae3735eafe99a9e61488c9b1298d7f0
huangqh <huangqh>
2021-09-14 14:50:11 +0800

    优化代码

--- app/src/main/java/com/hong/android_com/AppApplication.java
+++ app/src/main/java/com/hong/android_com/AppApplication.java
@@ -2,7 +2,6 @@ package com.hong.android_com;
 
 
 import com.alibaba.android.arouter.launcher.ARouter;
-import com.hong.library_base.config.ModuleLifecycleConfig;
 
 import me.goldze.mvvmhabit.base.BaseApplication;
 
@@ -15,10 +14,5 @@ public class AppApplication extends BaseApplication {
             ARouter.openDebug();   // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
         }
         ARouter.init(this);
-        //初始化组件(靠前)
-        ModuleLifecycleConfig.getInstance().initModuleAhead(this);
-        //....
-        //初始化组件(靠后)
-        ModuleLifecycleConfig.getInstance().initModuleLow(this);
     }
 }
--- library-base/src/main/java/com/hong/library_base/debug/DebugApplication.java
+++ library-base/src/main/java/com/hong/library_base/debug/DebugApplication.java
@@ -1,7 +1,6 @@
 package com.hong.library_base.debug;
 
 
-import com.hong.library_base.config.ModuleLifecycleConfig;
 
 import me.goldze.mvvmhabit.base.BaseApplication;
 
@@ -14,10 +13,5 @@ public class DebugApplication extends BaseApplication {
     @Override
     public void onCreate() {
         super.onCreate();
-        //初始化组件(靠前)
-        ModuleLifecycleConfig.getInstance().initModuleAhead(this);
-        //....
-        //初始化组件(靠后)
-        ModuleLifecycleConfig.getInstance().initModuleLow(this);
     }
 }
